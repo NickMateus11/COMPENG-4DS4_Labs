13a080336d4685b521ae98b39042f2ee5e1481c9
michael <skellsm@mcmcmaster.ca>
2022-03-11 16:20:09 -0500

    started terminal

--- Project/Project1/frdmk66f_Project1/source/Terminal_Component.c
+++ Project/Project1/frdmk66f_Project1/source/Terminal_Component.c
@@ -3,6 +3,11 @@
 EventGroupHandle_t event_group;
 QueueHandle_t uart_queue;
 
+#define LEFT_BIT (1 << 0)
+#define RIGHT_BIT (1 << 1)
+#define UP_BIT (1 << 2)
+#define DOWN_BIT (1 << 3)
+
 void setupTerminalComponent()
 {
 	BaseType_t status;
@@ -27,7 +32,14 @@ void setupTerminalComponent()
 
     /*************** Terminal Control Task ***************/
     //Create Event groups
+    event_group = xEventGroupCreate();
 	//Create Terminal Control Task
+	status = xTaskCreate(terminalControlTask, "terminalControlTask", 200, (void*)event_group, 2, NULL);
+	if (status != pdPASS)
+	{
+		PRINTF("Task creation failed!.\r\n");
+		while (1);
+	}
 }
 
 void setupTerminalPins()
@@ -56,6 +68,21 @@ void sendMessage(const char *format, ...)
 void setupUART()
 {
 	//Setup UART for sending and receiving
+	uart_config_t config;
+	UART_GetDefaultConfig(&config);
+
+	config.baudRate_Bps = 57600;
+	config.enableTx = true;
+	config.enableRx = true;
+	config.enableRxRTS = true;
+	config.enableTxCTS = true;
+
+	UART_Init(TARGET_UART, &config, CLOCK_GetFreq(kCLOCK_BusClk));
+
+	/******** Enable Interrupts *********/
+	UART_EnableInterrupts(TARGET_UART, kUART_RxDataRegFullInterruptEnable);
+	NVIC_SetPriority(UART4_RX_TX_IRQn, 2);
+	EnableIRQ(UART4_RX_TX_IRQn);
 }
 
 void uartTask(void* pvParameters)
@@ -81,10 +108,53 @@ void uartTask(void* pvParameters)
 
 void UART4_RX_TX_IRQHandler()
 {
-	//UART ISR
+	BaseType_t xHigherPriorityTaskWoken = pdFALSE;
+		UART_GetStatusFlags(TARGET_UART);
+
+		char ch = UART_ReadByte(TARGET_UART);
+		switch (ch) {
+			case 'a':
+				xEventGroupSetBitsFromISR(event_group, LEFT_BIT, &xHigherPriorityTaskWoken);
+			break;
+			case 's':
+				xEventGroupSetBitsFromISR(event_group, DOWN_BIT, &xHigherPriorityTaskWoken);
+			break;
+			case 'd':
+				xEventGroupSetBitsFromISR(event_group, RIGHT_BIT, &xHigherPriorityTaskWoken);
+			break;
+			case 'w':
+				xEventGroupSetBitsFromISR(event_group, UP_BIT, &xHigherPriorityTaskWoken);
+			break;
+		}
+		portYIELD_FROM_ISR(xHigherPriorityTaskWoken);
 }
 
 void terminalControlTask(void* pvParameters)
 {
 	//Terminal Control Task implementation
+	EventGroupHandle_t event_group = (EventGroupHandle_t) pvParameters;
+		EventBits_t bits;
+
+		while (1) {
+			bits = xEventGroupWaitBits(
+						event_group,
+						LEFT_BIT | RIGHT_BIT | UP_BIT | DOWN_BIT,
+						pdTRUE,
+						pdFALSE,
+						portMAX_DELAY);
+
+			if ((bits & LEFT_BIT) == LEFT_BIT) {
+				PRINTF("Left\r\n");
+			}
+			if ((bits & RIGHT_BIT) == RIGHT_BIT) {
+				PRINTF("Right\r\n");
+			}
+			if ((bits & UP_BIT) == UP_BIT) {
+				PRINTF("Up\r\n");
+			}
+			if ((bits & DOWN_BIT) == DOWN_BIT) {
+				PRINTF("Down\r\n");
+			}
+		}
+
 }
