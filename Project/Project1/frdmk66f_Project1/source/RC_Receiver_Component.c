6ecf68decad39e602dc5d3205bdf4f6e1a147b63
zan-d <53127723+zan-d@users.noreply.github.com>
2022-03-11 17:50:48 -0500

    updated rc for dc and servo motor

--- Project/Project1/frdmk66f_Project1/source/RC_Receiver_Component.c
+++ Project/Project1/frdmk66f_Project1/source/RC_Receiver_Component.c
@@ -3,6 +3,18 @@
 SemaphoreHandle_t rc_hold_semaphore;
 TaskHandle_t rc_task_handle;
 
+typedef struct {
+	uint16_t header;
+	uint16_t ch1;
+	uint16_t ch2;
+	uint16_t ch3;
+	uint16_t ch4;
+	uint16_t ch5;
+	uint16_t ch6;
+	uint16_t ch7;
+	uint16_t ch8;
+} RC_Values;
+
 void setupRCReceiverComponent()
 {
 	BaseType_t status;
@@ -12,8 +24,8 @@ void setupRCReceiverComponent()
 
     /*************** RC Task ***************/
 	//Create RC Semaphore
-	SemaphoreHandle_t* rc_hold_semaphore = (SemaphoreHandle_t*) malloc(3 * sizeof(SemaphoreHandle_t));
-	rc_hold_semaphore[0] = xSemaphoreCreateBinary();
+	SemaphoreHandle_t* rc_hold_semaphore = (SemaphoreHandle_t*) malloc(1 * sizeof(SemaphoreHandle_t));
+	rc_hold_semaphore = xSemaphoreCreateBinary();
 
 	//Create RC Task
 	status = xTaskCreate(rcTask, "rc", 200, (void*)rc_hold_semaphore, 2, NULL);
@@ -51,7 +63,69 @@ void setupUART_RC()
 void rcTask(void* pvParameters)
 {
 	//RC task implementation
+	SemaphoreHandle_t* semaphores = (SemaphoreHandle_t*)pvParameters;
+	SemaphoreHandle_t hold_semaphore = semaphores;
 
+	BaseType_t status;
+	uart_config_t config;
+	RC_Values rc_values;
+	uint8_t* ptr = (uint8_t*) &rc_values;
+	int motor, angle;
+
+	config.baudRate_Bps = 115200;
+	config.enableTx = false;
+	config.enableRx = true;
+
+	UART_Init(UART1, &config, CLOCK_GetFreq(kCLOCK_CoreSysClk));
+
+	while(1)
+	{
+		status = xSemaphoreTake(hold_semaphore, portMAX_DELAY);
+		if (status != pdPASS)
+		{
+			PRINTF("Failed to acquire hold_semaphore\r\n");
+			while (1);
+		}
+
+		UART_ReadBlocking(UART1, ptr, 1);
+		if(*ptr != 0x20)
+			continue;
+
+		UART_ReadBlocking(UART1, &ptr[1], sizeof(rc_values) - 1);
+		if(rc_values.header == 0x4020)
+		{
+
+			//right joy stick for forward/backward
+//			printf("Channel 1 = %d\t", rc_values.ch1);
+			printf("Channel 2 = %d\t", rc_values.ch2);
+			motor = (int)(rc_values.ch2 * 1.0f/5.0f - 300);
+
+			status = xQueueSendToBack(motor_queue, (void*) &motor, portMAX_DELAY);
+			if (status != pdPASS)
+			{
+				PRINTF("Queue Send failed!.\r\n");
+				while (1);
+			}
+
+			//left joy stick for left/right
+//			printf("Channel 3 = %d\t", rc_values.ch3);
+			printf("Channel 4 = %d\t", rc_values.ch4);
+			angle = (int)(rc_values.ch4 * 1.0f/5.0f - 300);
+
+			status = xQueueSendToBack(angle_queue, (void*) &angle, portMAX_DELAY);
+			if (status != pdPASS)
+			{
+				PRINTF("Queue Send failed!.\r\n");
+				while (1);
+			}
+
+//			printf("Channel 5 = %d\t", rc_values.ch5);
+//			printf("Channel 6 = %d\t", rc_values.ch6);
+//			printf("Channel 7 = %d\t", rc_values.ch7);
+//			printf("Channel 8 = %d\r\n", rc_values.ch8);
+		}
+		xSemaphoreGive(hold_semaphore);
+	}
 }
 
 
